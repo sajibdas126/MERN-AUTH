120bca303553be54490a5092ba33f0531ee5bc8d
Sajib <sajibdashtrg@gmail.com>
2025-12-22 00:06:39 +0600

    Add Header component and enhance Navbar and Login pages with improved layout and functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate = useNavigate()
   return (
     <div>
-        <img src={assets.logo} className='w-28 sm:w-32'/>
+       <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px24 absolute top-0'>
+         <img src={assets.logo} className='w-28 sm:w-32'/>
+        <button onClick={()=>navigate('/login')}
+        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all '>Login <img src={assets.arrow_icon} alt="" /></button>
+       </div>
 
-        <button>Login</button>
     </div>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const Login = () => {
+ 
+  const [state, setState] = useState("Sign Up")
+
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+      <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
+      
+      <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-sm text-indigo-300'>
+          
+          
+          <h2 className='text-3xl font-semibold text-white text-center mb-3'>
+            {state === 'Sign Up' ? 'Create account' : 'Login'}
+          </h2>
+          
+          <p className='text-center text-sm mb-6'>
+            {state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}
+          </p>
+      
+          <form>
+           
+            {state === 'Sign Up' && (
+              <div className='flex items-center gap-3 mb-4 w-full px-5 py-2 rounded-full bg-[#333A5c]'>
+                <img src={assets.person_icon} alt="" />
+                <input className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
+              </div>
+            )}
+            
+            <div className='flex items-center gap-3 mb-4 w-full px-5 py-2 rounded-full bg-[#333A5c]'>
+              <img src={assets.mail_icon} alt="" />
+              <input className='bg-transparent outline-none text-white' type="email" placeholder='Email id' required />
+            </div>
+
+            <div className='flex items-center gap-3 mb-4 w-full px-5 py-2 rounded-full bg-[#333A5c]'>
+              <img src={assets.lock_icon} alt="" />
+              <input className='bg-transparent outline-none text-white' type="password" placeholder='Password' required />
+            </div>
+
+           
+            {state === 'Login' && (
+               <p className='mb-4 text-indigo-500 cursor-pointer'>Forgot password?</p>
+            )}
+
+            <button className='w-full py-2.5 rounded-full text-white font-medium bg-gradient-to-r from-indigo-500 to-indigo-900 cursor-pointer'>
+              {state}
+            </button>
+          </form>
+
+          
+          {state === 'Sign Up' ? (
+            <p className='text-center text-gray-400 text-xs mt-4'>
+              Already have an account?{" "} 
+              <span onClick={() => setState('Login')} className='text-blue-400 cursor-pointer underline'>
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className='text-center text-gray-400 text-xs mt-4'>
+              Don't have an account?{" "} 
+              <span onClick={() => setState('Sign Up')} className='text-blue-400 cursor-pointer underline'>
+                Sign up
+              </span>
+            </p>
+          )}
+      </div>
+    </div>
   )
 }
 
