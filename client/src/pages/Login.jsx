feaeeda7655cb0c3ac57afe38db2f8a43310865a
Sajib <sajibdashtrg@gmail.com>
2025-12-23 12:06:17 +0600

    Implement user authentication context and enhance Login functionality with toast notifications

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,56 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
+import {useNavigate} from 'react-router-dom'
+import { AppContent } from '../context/AppContext'
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 const Login = () => {
+
+  const navigate =useNavigate()
+
+  const {backendUrl,setIsLoggedin,getUserData} = useContext(AppContent)
  
   const [state, setState] = useState("Sign Up")
+  const [name,setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+
+  const onSubmitHandler = async (e) =>{
+    try {
+      e.preventDefault();
+
+      axios.defaults.withCredentials = true
+
+      if (state === "Sign Up"){
+       const { data } = await axios.post(`${backendUrl}/api/auth/register`, { name, email, password });
+       if(data.success){
+        setIsLoggedin(true)
+        getUserData()
+        navigate("/")
+       }else{
+        toast.error(Date.message)
+       }
+      }else{
+        const {data} = await axios.post(backendUrl + '/api/auth/login',{email,password})
+
+       if(data.success){
+        setIsLoggedin(true)
+        getUserData()
+        navigate("/")
+       }else{
+        toast.error(Date.message)
+       }
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
 
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
-      <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
+      <img onClick={()=> navigate ("/")} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
       
       <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-sm text-indigo-300'>
           
@@ -20,29 +63,37 @@ const Login = () => {
             {state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}
           </p>
       
-          <form>
+          <form onSubmit={onSubmitHandler}>
            
             {state === 'Sign Up' && (
               <div className='flex items-center gap-3 mb-4 w-full px-5 py-2 rounded-full bg-[#333A5c]'>
                 <img src={assets.person_icon} alt="" />
-                <input className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
+                <input 
+                onChange={e => setName(e.target.value)} 
+                value={name} className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
               </div>
             )}
             
             <div className='flex items-center gap-3 mb-4 w-full px-5 py-2 rounded-full bg-[#333A5c]'>
               <img src={assets.mail_icon} alt="" />
-              <input className='bg-transparent outline-none text-white' type="email" placeholder='Email id' required />
+              <input 
+               onChange={e => setEmail(e.target.value)} 
+                value={email}
+              className='bg-transparent outline-none text-white' type="email" placeholder='Email id' required />
             </div>
 
             <div className='flex items-center gap-3 mb-4 w-full px-5 py-2 rounded-full bg-[#333A5c]'>
               <img src={assets.lock_icon} alt="" />
-              <input className='bg-transparent outline-none text-white' type="password" placeholder='Password' required />
+              <input 
+               onChange={e => setPassword(e.target.value)} 
+              value={password}
+              className='bg-transparent outline-none text-white' type="password" placeholder='Password' required />
             </div>
 
            
-            {state === 'Login' && (
-               <p className='mb-4 text-indigo-500 cursor-pointer'>Forgot password?</p>
-            )}
+            
+               <p onClick={()=>navigate('/reset-password')} className='mb-4 text-indigo-500 cursor-pointer'>Forgot password?</p>
+            
 
             <button className='w-full py-2.5 rounded-full text-white font-medium bg-gradient-to-r from-indigo-500 to-indigo-900 cursor-pointer'>
               {state}
